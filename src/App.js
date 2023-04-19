ae65c4c4b5fc397f0a69c30ec0393861076ef791
Taiwo Ayoola <tayoola13@yahoo.com>
2023-04-19 23:59:32 +0100

    Added login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import { ChatEngine } from 'react-chat-engine';
 import ChatFeed from './components/ChatFeed/ChatFeed';
 import './App.css';
+import Login from './components/Onboarding/Login';
 
 const App = () => {
 	return (
+		<div>
+			<Login />
 		<ChatEngine
 			height="100vh"
 			projectID="ec546b40-27a4-4928-9503-05a232ef2d19" 
@@ -11,6 +14,7 @@ const App = () => {
 			userSecret="111111"
 			renderChatFeed={(chatAppProps)=> <ChatFeed {...chatAppProps} />}
 		/>
+		</div>
 	)
 }
 
