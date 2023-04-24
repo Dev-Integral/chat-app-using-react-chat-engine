82438665cdcc5e7cd38a0e961ce4691f592045b2
Taiwo Ayoola <tayoola13@yahoo.com>
2023-04-24 13:28:26 +0100

    Completed the login flow

--- src/App.js
+++ src/App.js
@@ -4,15 +4,15 @@ import './App.css';
 import Login from './components/Onboarding/Login';
 
 const App = () => {
+	if(!localStorage.getItem('username')) return <Login />
 	return (
 		<div>
-			<Login />
 		<ChatEngine
 			height="100vh"
 			projectID="ec546b40-27a4-4928-9503-05a232ef2d19" 
-			userName="test1"
-			userSecret="111111"
-			renderChatFeed={(chatAppProps)=> <ChatFeed {...chatAppProps} />}
+			userName={localStorage.getItem('username')}
+			userSecret={localStorage.getItem('password')}
+			// renderChatFeed={(chatAppProps)=> <ChatFeed {...chatAppProps} />}
 		/>
 		</div>
 	)
--- src/components/Onboarding/Login.jsx
+++ src/components/Onboarding/Login.jsx
@@ -4,22 +4,34 @@ import axios from "axios";
 const Login = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
-    const handleSubmit =(e)=> {
+    const [error, setError] = useState('');
+
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        const authObject = {'Project-ID': "ec546b40-27a4-4928-9503-05a232ef2d19", "User-Name": username, "User-Secret": password}
+        const authObject = { 'Project-ID': "ec546b40-27a4-4928-9503-05a232ef2d19", "User-Name": username, "User-Secret": password }
+
+        try {
+            await axios.get('https://api.chatengine.io/chats', { headers: authObject });
+            localStorage.setItem('username', username);
+            localStorage.setItem('password', password);
+            window.location.reload();
+        } catch (error) {
+            setError("Oops, incorrect credentials");
+        }
     }
     return (
         <div className="wrapper">
             <div className="form">
                 <h1 className="title">Chat Application</h1>
                 <form onSubmit={handleSubmit}>
-                    <input type={'text'} />
-                    <input type={'password'} onChange={(e)=>setPassword(e.target.value)}  /> 
+                    <input type={'text'} onChange={(e) => setUsername(e.target.value)} value={username} className="input" placeholder="username" required />
+                    <input type={'password'} onChange={(e) => setPassword(e.target.value)} value={password} className="input" placeholder="password" required />
                     <div align="center">
                         <button type="submit" className="button">
-
+                            <span>Start Chatting</span>
                         </button>
                     </div>
+                    <h2 className="error">{error}</h2>
                 </form>
             </div>
         </div>
